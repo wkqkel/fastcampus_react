54fb802a6474bace790229bc7f0500a4dc88f94b
sangwon <wkqkel@naver.com>
2024-04-12 22:13:13 +0900

    ButtonGroup 컴포넌트

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -5,7 +5,7 @@ import { ApplyValues } from '@/models/apply'
 import { useState } from 'react'
 
 function ApplyPage() {
-  const [step, setStep] = useState(1)
+  const [step, setStep] = useState(2)
 
   const handleTermsChange = (terms: ApplyValues['terms']) => {
     console.log(terms)
@@ -15,11 +15,17 @@ function ApplyPage() {
     infoValues: Pick<ApplyValues, 'salary' | 'payDate' | 'creditScore'>,
   ) => {}
 
+  const handleCardInfoChange = (
+    cardInfoValues: Pick<ApplyValues, 'isHipass' | 'isRf' | 'isMaster'>,
+  ) => {
+    console.log(cardInfoValues)
+  }
+
   return (
     <div>
       {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
       {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
-      {step === 2 ? <CardInfo /> : null}
+      {step === 2 ? <CardInfo onNext={handleCardInfoChange} /> : null}
     </div>
   )
 }
