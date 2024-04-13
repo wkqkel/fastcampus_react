5f1b90383589077f650e29cb3aa64695414bda48
sangwon <wkqkel@naver.com>
2024-04-13 19:01:28 +0900

    이탈시 step기록

--- src/components/apply/index.tsx
+++ src/components/apply/index.tsx
@@ -7,31 +7,45 @@ import { useParams } from 'react-router-dom'
 import useUser from '../hooks/auth/useUser'
 
 function Apply({ onSubmit }: { onSubmit: (applyValues: ApplyValues) => void }) {
-  const [step, setStep] = useState(0)
   const user = useUser()
   const { id } = useParams() as { id: string }
-  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>({
-    userId: user?.uid,
-    cardId: id,
+
+  const storageKey = `applied-${user?.uid}-${id}`
+
+  const [applyValues, setApplyValues] = useState<Partial<ApplyValues>>(() => {
+    const applied = localStorage.getItem(storageKey)
+
+    if (applied == null) {
+      return {
+        userId: user?.uid,
+        cardId: id,
+        step: 0,
+      }
+    }
+
+    return JSON.parse(applied)
   })
 
   useEffect(() => {
-    if (step === 3) {
+    if (applyValues.step === 3) {
+      localStorage.removeItem(storageKey)
+
       onSubmit({
         ...applyValues,
         appliedAt: new Date(),
         status: APPLY_STATUS.READY,
       } as ApplyValues)
+    } else {
+      localStorage.setItem(storageKey, JSON.stringify(applyValues))
     }
-  }, [applyValues, onSubmit, step])
+  }, [applyValues, onSubmit, storageKey])
 
   const handleTermsChange = (terms: ApplyValues['terms']) => {
     setApplyValues((prevValues) => ({
       ...prevValues,
       terms,
+      step: (prevValues.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
 
   const handleBasicInfoChange = (
@@ -40,9 +54,8 @@ function Apply({ onSubmit }: { onSubmit: (applyValues: ApplyValues) => void }) {
     setApplyValues((prevValues) => ({
       ...prevValues,
       ...infoValues,
+      step: (prevValues.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
 
   const handleCardInfoChange = (
@@ -51,16 +64,19 @@ function Apply({ onSubmit }: { onSubmit: (applyValues: ApplyValues) => void }) {
     setApplyValues((prevValues) => ({
       ...prevValues,
       ...cardInfoValues,
+      step: (prevValues.step as number) + 1,
     }))
-
-    setStep((prevStep) => prevStep + 1)
   }
 
   return (
     <div>
-      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
-      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
-      {step === 2 ? <CardInfo onNext={handleCardInfoChange} /> : null}
+      {applyValues.step === 0 ? <Terms onNext={handleTermsChange} /> : null}
+      {applyValues.step === 1 ? (
+        <BasicInfo onNext={handleBasicInfoChange} />
+      ) : null}
+      {applyValues.step === 2 ? (
+        <CardInfo onNext={handleCardInfoChange} />
+      ) : null}
     </div>
   )
 }
