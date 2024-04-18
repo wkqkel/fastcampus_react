c51eff29082e892f3f55d6241c1d538918cefe0f
sangwon <wkqkel@naver.com>
2024-04-18 14:20:24 +0900

    setting

---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,54 @@
+import { css } from '@emotion/react'
+import { Link, useLocation } from 'react-router-dom'
+
+import { colors } from '@/styles/colorPalette'
+import Flex from '@shared/Flex'
+import Button from './Button'
+import { useCallback } from 'react'
+
+function Navbar() {
+  const location = useLocation()
+  const showSignButton =
+    ['/signup', 'signin'].includes(location.pathname) === false
+
+  // @TODO
+  const user = null
+
+  const renderButton = useCallback(() => {
+    if (user != null) {
+      return (
+        <Link to="/my">
+          {/* @TODO */}
+          <img src="" alt="" />
+        </Link>
+      )
+    }
+
+    if (showSignButton) {
+      return (
+        <Link to="/signin">
+          <Button>로그인/회원가입</Button>
+        </Link>
+      )
+    }
+
+    return null
+  }, [showSignButton, user])
+
+  return (
+    <Flex align="center" justify="space-between" css={navbarContainerStyles}>
+      <Link to="/">홈</Link>
+      {renderButton()}
+    </Flex>
+  )
+}
+
+const navbarContainerStyles = css`
+  padding: 10px 24px;
+  position: sticky;
+  top: 0;
+  background-color: ${colors.white};
+  z-index: 10;
+  border-bottom: 1px solid ${colors.gray};
+`
+export default Navbar
